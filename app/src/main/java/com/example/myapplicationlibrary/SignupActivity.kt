fc389d4b29bb79ed27b57acbe82b0ae5dcf90e69
Ritika Shrestha <shrestharitika998@gmail.com>
2025-01-15 14:31:52 +0545

    Profile Activity

--- app/src/main/java/com/example/myapplicationlibrary/SignupActivity.kt
+++ app/src/main/java/com/example/myapplicationlibrary/SignupActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplicationlibrary
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -24,10 +25,29 @@ class SignupActivity : AppCompatActivity() {
 
 
         bind.signupbtn.setOnClickListener {
+            val username: String=bind.name.text.toString()
+            val email: String=bind.email.text.toString()
+            val password: String=bind.password.text.toString()
+            val conpassword: String=bind.confirmpassword.text.toString()
+            if (username.isEmpty()){
+                Toast.makeText(this@SignupActivity,"Username can't be empty", Toast.LENGTH_LONG).show()
+            }else if(email.isEmpty()){
+                Toast.makeText(this@SignupActivity,"Email can't be empty", Toast.LENGTH_LONG).show()
+            }else if(password.isEmpty()){
+                Toast.makeText(this@SignupActivity,"Password can't be empty", Toast.LENGTH_LONG).show()
+            }else if(conpassword.isEmpty()){
+                Toast.makeText(this@SignupActivity,"Confirm Password can't be empty", Toast.LENGTH_LONG).show()
+            }else if(password!=conpassword){
+                Toast.makeText(this@SignupActivity,"Password and Confirm Password should be same",
+                    Toast.LENGTH_LONG).show()
 
-
-
+            }
+//            else{
+//                val intent =Intent(this@SignupActivity,LoginActivityyy::class.java)
+//                startActivity(intent)
+//            }
         }
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())

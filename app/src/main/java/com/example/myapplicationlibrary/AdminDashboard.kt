bcd5bd86db2589ce1fa7e713109e7748bd2fd55a
shreya <shreyakhadka007@gmail.com>
2025-03-01 17:23:01 +0545

    Working on admin dashboard activity

--- app/src/main/java/com/example/myapplicationlibrary/AdminDashboard.kt
+++ app/src/main/java/com/example/myapplicationlibrary/AdminDashboard.kt
@@ -1,20 +1,44 @@
 package com.example.myapplicationlibrary
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplicationlibrary.databinding.ActivityAdminDashboardBinding
+
 
 class AdminDashboard : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAdminDashboardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_admin_dashboard)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Initialize View Binding
+        binding = ActivityAdminDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        // Set click listeners for buttons
+//        binding.addbutton.setOnClickListener {
+//            // Navigate to AddBookActivity
+//            startActivity(Intent(this, AddBookActivity::class.java))
+//        }
+
+//        binding.updatebutton.setOnClickListener {
+//            // Navigate to UpdateBookActivity
+//            startActivity(Intent(this, UpdateBookActivity::class.java))
+//        }
+
+        binding.addcategorybutton.setOnClickListener {
+            // Navigate to AddCategoryActivity
+            startActivity(Intent(this, AddCategoryActivity::class.java))
         }
+
+//        binding.viewrequestbutton.setOnClickListener {
+//            // Navigate to ViewRequestActivity
+//            startActivity(Intent(this, ViewRequestActivity::class.java))
+//        }
     }
 }

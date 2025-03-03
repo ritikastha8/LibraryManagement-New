817d6ab3c4849e44430a1336db4c07c91eb2e407
Ritika Shrestha <shrestharitika998@gmail.com>
2025-03-03 22:43:42 +0545

    Update Request Activity

--- app/src/main/java/com/example/myapplicationlibrary/UpdateRequestActivity.kt
+++ app/src/main/java/com/example/myapplicationlibrary/UpdateRequestActivity.kt
@@ -14,7 +14,7 @@ import com.google.firebase.database.FirebaseDatabase
 class UpdateRequestActivity : AppCompatActivity() {
     lateinit var binding:ActivityUpdateRequestBinding
     lateinit var requestViewModel: RequestViewModel
-
+//
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()

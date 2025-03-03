007c5a8e3c1466d52aced882d086b1ba276c9306
Ritika Shrestha <shrestharitika998@gmail.com>
2025-03-03 22:41:47 +0545

    Return book .kt

--- app/src/main/java/com/example/myapplicationlibrary/ReturnActivity.kt
+++ app/src/main/java/com/example/myapplicationlibrary/ReturnActivity.kt
@@ -10,6 +10,7 @@ class ReturnActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        //
         setContentView(R.layout.activity_return)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
